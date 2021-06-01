9f42d35bc3daae7acaac3b83b70618b4fd8364a9
Edgar Carrillo <ecarrillo046@gmail.com>
2021-06-01 09:29:36 -0700

    Add function that pushes user input book to an array of books

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,13 @@ function Book(title, author, numOfPages, readConfirm) {
     this.author = author;
     this.numOfPages = numOfPages;
     this.readConfirm = readConfirm;
-    this.info = `${title} by ${author}, ${numOfPages} pages, ${read}`;
+    this.info = `${title} by ${author}, ${numOfPages} pages, ${readConfirm}`;
 }
 
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
+
+let bookArr = [theHobbit];
+console.log(bookArr);
+function addBookToLibrary(bookArr, userBook) {
+    bookArr.push(book);
+}
