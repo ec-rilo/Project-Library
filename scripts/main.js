8d82b3c9acd0228dd8c5d093f7ae0774390ad9e6
Edgar Carrillo <ecarrillo046@gmail.com>
2021-06-01 13:25:45 -0700

    Add function that displays book cards to webpage

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,57 @@ function Book(title, author, numOfPages, bookLang, publishDate, readConfirm) {
     this.readConfirm = readConfirm;
 }
 
-let GoT = new Book('A Game Of Thrones', 'George R. R. Martin', '694', 'English', 'publishDate', 'not read yet');
+let GoT = new Book('A Game Of Thrones', 'George R. R. Martin', '694', 'English', 'July 1, 1996', 'not read yet');
 
 let bookArr = [GoT];
 function addBookToLibrary(bookArr, userBook) {
     bookArr.push(book);
 }
+
+
+const booksContainer = document.querySelector('.books-container');
+function displayBooks() {
+    for (let i = 0; i < bookArr.length; ++i) {
+        let bookCardContainer = document.createElement('div');
+        bookCardContainer.classList.add(`book${i + 1}`);
+        bookCardContainer.classList.add('card-container');
+
+        let rmCardBtn = document.createElement('div');
+        rmCardBtn.classList.add('remove-card-btn');
+        rmCardBtn.innerHTML = 'x';
+        bookCardContainer.appendChild(rmCardBtn);
+
+        let cardTextContainer = document.createElement('ul');
+        cardTextContainer.classList.add('card-text-container');
+
+        let bookTitle = document.createElement('li');
+        bookTitle.classList.add('card-book-title');
+        bookTitle.innerHTML = bookArr[i].title;
+        cardTextContainer.appendChild(bookTitle);
+
+        let authorInfo = document.createElement('li');
+        authorInfo.classList.add('card-content');
+        authorInfo.innerHTML = 'By: ' + bookArr[i].author;
+        cardTextContainer.appendChild(authorInfo);
+
+        let pageInfo = document.createElement('li');
+        pageInfo.classList.add('card-content');
+        pageInfo.innerHTML = 'Number of pages: ' + bookArr[i].numOfPages;
+        cardTextContainer.appendChild(pageInfo);
+
+        let bookLangInfo = document.createElement('li');
+        bookLangInfo.classList.add('card-content');
+        bookLangInfo.innerHTML = 'Language: ' + bookArr[i].bookLang;
+        cardTextContainer.appendChild(bookLangInfo);
+
+        let bookPublishDate = document.createElement('li');
+        bookPublishDate.classList.add('card-content');
+        bookPublishDate.innerHTML = 'Published: ' + bookArr[i].publishDate;
+        cardTextContainer.appendChild(bookPublishDate);
+
+        bookCardContainer.appendChild(cardTextContainer);
+        booksContainer.appendChild(bookCardContainer);
+    }
+}
+
+displayBooks();
