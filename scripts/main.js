4e00e6a2422ee29bbaf70009bceb7f290f07d003
Edgar Carrillo <ecarrillo046@gmail.com>
2021-06-04 12:27:43 -0700

    Add a function that creates a single book card when called

--- scripts/main.js
+++ scripts/main.js
@@ -17,70 +17,71 @@ let aHabits = new Book('Atomic Habits', 'James Clear', '322', 'English', 'Date #
 let congo = new Book('Congo', 'Micheal Crichton', '339','English', 'Date #, ####', 'Read')
 
 let bookArr = [GoT, aHabits, congo];
-function addBookToLibrary(book) {
-    bookArr.push(book);
-}
 
 const booksContainer = document.querySelector('.books-container');
 function displayBooks() {
     for (let i = 0; i < bookArr.length; ++i) {
-        let bookCardContainer = document.createElement('div');
-        bookCardContainer.classList.add('card-container');
-        bookCardContainer.dataset.bookNum = `${i}`;
-
-        let rmCardBtn = document.createElement('button');
-        rmCardBtn.classList.add('remove-card-btn');
-        rmCardBtn.setAttribute('type', 'button');
-        rmCardBtn.innerHTML = 'x';
-        rmCardBtn.dataset.bookObjIndex = `${i}`;
-        bookCardContainer.appendChild(rmCardBtn);
-
-        let cardTextContainer = document.createElement('ul');
-        cardTextContainer.classList.add('card-text-container');
-
-        let bookTitle = document.createElement('li');
-        bookTitle.classList.add('card-book-title');
-        bookTitle.innerHTML = bookArr[i].title;
-        cardTextContainer.appendChild(bookTitle);
-
-        let authorInfo = document.createElement('li');
-        authorInfo.classList.add('card-content');
-        authorInfo.innerHTML = 'By: ' + bookArr[i].author;
-        cardTextContainer.appendChild(authorInfo);
-
-        let pageInfo = document.createElement('li');
-        pageInfo.classList.add('card-content');
-        pageInfo.innerHTML = 'Number of pages: ' + bookArr[i].numOfPages;
-        cardTextContainer.appendChild(pageInfo);
-
-        let bookLangInfo = document.createElement('li');
-        bookLangInfo.classList.add('card-content');
-        bookLangInfo.innerHTML = 'Language: ' + bookArr[i].bookLang;
-        cardTextContainer.appendChild(bookLangInfo);
-
-        let bookPublishDate = document.createElement('li');
-        bookPublishDate.classList.add('card-content');
-        bookPublishDate.innerHTML = 'Published: ' + bookArr[i].publishDate;
-        cardTextContainer.appendChild(bookPublishDate);
-
-        let toggleMaster = document.createElement('div');
-        toggleMaster.setAttribute('class', 'toggle-master');
-        let toggleLabel = document.createElement('label');
-        toggleLabel.setAttribute('for', 'toggle-container');
-        toggleLabel.innerHTML = 'Mark as read: ';
-        toggleMaster.appendChild(toggleLabel);
-        let toggleContainer = document.createElement('input');
-        toggleContainer.setAttribute('type', 'checkbox');
-        toggleContainer.setAttribute('id', 'toggle-container');
-        toggleMaster.appendChild(toggleContainer);
-        cardTextContainer.appendChild(toggleMaster);
-
-        bookCardContainer.appendChild(cardTextContainer);
-        booksContainer.appendChild(bookCardContainer);
+        createBook();
     }
 }
 
 displayBooks();
+
+function createBook() {
+    let bookCardContainer = document.createElement('div');
+    bookCardContainer.classList.add('card-container');
+    bookCardContainer.dataset.bookNum = `${i}`;
+
+    let rmCardBtn = document.createElement('button');
+    rmCardBtn.classList.add('remove-card-btn');
+    rmCardBtn.setAttribute('type', 'button');
+    rmCardBtn.innerHTML = 'x';
+    rmCardBtn.dataset.bookObjIndex = `${i}`;
+    bookCardContainer.appendChild(rmCardBtn);
+
+    let cardTextContainer = document.createElement('ul');
+    cardTextContainer.classList.add('card-text-container');
+
+    let bookTitle = document.createElement('li');
+    bookTitle.classList.add('card-book-title');
+    bookTitle.innerHTML = bookArr[i].title;
+    cardTextContainer.appendChild(bookTitle);
+
+    let authorInfo = document.createElement('li');
+    authorInfo.classList.add('card-content');
+    authorInfo.innerHTML = 'By: ' + bookArr[i].author;
+    cardTextContainer.appendChild(authorInfo);
+
+    let pageInfo = document.createElement('li');
+    pageInfo.classList.add('card-content');
+    pageInfo.innerHTML = 'Number of pages: ' + bookArr[i].numOfPages;
+    cardTextContainer.appendChild(pageInfo);
+
+    let bookLangInfo = document.createElement('li');
+    bookLangInfo.classList.add('card-content');
+    bookLangInfo.innerHTML = 'Language: ' + bookArr[i].bookLang;
+    cardTextContainer.appendChild(bookLangInfo);
+
+    let bookPublishDate = document.createElement('li');
+    bookPublishDate.classList.add('card-content');
+    bookPublishDate.innerHTML = 'Published: ' + bookArr[i].publishDate;
+    cardTextContainer.appendChild(bookPublishDate);
+
+    let toggleMaster = document.createElement('div');
+    toggleMaster.setAttribute('class', 'toggle-master');
+    let toggleLabel = document.createElement('label');
+    toggleLabel.setAttribute('for', 'toggle-container');
+    toggleLabel.innerHTML = 'Mark as read: ';
+    toggleMaster.appendChild(toggleLabel);
+    let toggleContainer = document.createElement('input');
+    toggleContainer.setAttribute('type', 'checkbox');
+    toggleContainer.setAttribute('id', 'toggle-container');
+    toggleMaster.appendChild(toggleContainer);
+    cardTextContainer.appendChild(toggleMaster);
+        
+    bookCardContainer.appendChild(cardTextContainer);
+    booksContainer.appendChild(bookCardContainer);
+}
 
 function removeCard() {
     let rmCardBtnArr = Array.from(document.querySelectorAll('.remove-card-btn'));
@@ -138,4 +139,8 @@ function closeForm() {
 
     let overlay = document.querySelector('.overlay');
     overlay.classList.remove('black-overlay');
+}
+
+function addBookToLibrary(book) {
+    bookArr.push(book);
 }
