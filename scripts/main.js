7b365102814f2b2a91d932d77e1fe11a0a6a1d01
Edgar Carrillo <ecarrillo046@gmail.com>
2021-06-28 11:56:55 -0700

    Refactor constructor to become a class

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,16 @@
 'use strict';
 
-
-function Book(title, author, numOfPages, bookLang, publishDate, readConfirm) {
-    this.title = title;
-    this.author = author;
-    this.numOfPages = numOfPages;
-    this.bookLang = bookLang;
-    this.publishDate = publishDate;
-    this.readConfirm = readConfirm;
+class Book {
+    constructor(title, author, numOfPages, bookLang, publishDate, readConfirm) {
+        this.title = title;
+        this.author = author;
+        this.numOfPages = numOfPages;
+        this.bookLang = bookLang;
+        this.publishDate = publishDate;
+        this.readConfirm = readConfirm;
+    }
 }
+
 
 let book1 = new Book('A Game Of Thrones', 'George R. R. Martin', '694', 'English', 'July 1, 1996', 'Not read yet');
 
